37dbd5c58b77fcdb5e816aa66740a2f1df4013f4
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2021-09-04 12:23:22 +0100

    Enable automatic Alpaca discovery to be disabled.

--- Conform/DataClasses/AlpacaConfiguration.cs
+++ Conform/DataClasses/AlpacaConfiguration.cs
@@ -7,6 +7,7 @@ namespace ConformU
 {
     public class AlpacaConfiguration
     {
+        public bool DiscoveryEnabled { get; set; } = true;
         public int NumberOfDiscoveryPolls { get; set; } = 1;
         public double DiscoveryPollInterval { get; set; } = 1.0;
         public int DiscoveryPort { get; set; } = 32227;
