09628f037eb7d1a4507a360bf834c16cf5fbd134
PeterSimpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-03-18 12:24:41 +0000

    Code sign Windows executables.

--- publishdebugnotrim.cmd
+++ publishdebugnotrim.cmd
@@ -1,5 +1,13 @@
-vcvarsall.bat x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 mkdir publish
+
+dotnet publish -c Debug /p:Platform="Any CPU" -r win-x64 --framework net6.0-windows --self-contained true /p:PublishTrimmed=false  -o ./publish/conformu.windows-x64
+signtool sign /a /v /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-x64\*.dll"
+signtool sign /a /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-x64\*.exe"
+
+7z a publish/conformu-windows-x64.zip publish\conformu.windows-x64\ 
+rmdir /s /q "publish/conformu.windows-x64"
 
 dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm --framework net6.0 --self-contained true -o ./publish/conformu.linux-arm32
 bsdtar -cJf publish/conformu.linux-arm32.tar.xz -C publish\conformu.linux-arm32 *
@@ -13,19 +21,21 @@ dotnet publish -c Debug /p:Platform="Any CPU" -r linux-x64 --framework net6.0 --
 bsdtar -cJf publish/conformu.linux-x64.tar.xz -C publish\conformu.linux-x64\ *
 rmdir /s /q "publish/conformu.linux-x64"
 
-dotnet publish -c Debug /p:Platform="Any CPU" -r win-x64 --framework net6.0-windows --self-contained true /p:PublishTrimmed=false  -o ./publish/conformu.windows-x64
-7z a publish/conformu-windows-x64.zip publish\conformu.windows-x64\ 
-rmdir /s /q "publish/conformu.windows-x64"
-
 dotnet publish -c Debug /p:Platform="Any CPU" -r win-x86 --framework net6.0-windows --self-contained true /p:PublishTrimmed=false -o ./publish/conformu.windows-x86
+signtool sign /a /v /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-x86\*.dll"
+signtool sign /a /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-x86\*.exe"
 7z a publish/conformu-windows-x86.zip publish\conformu.windows-x86\
 rmdir /s /q "publish/conformu.windows-x86"
 
 dotnet publish -c Debug /p:Platform="Any CPU" -r win-x86 --framework net6.0-windows --self-contained false /p:PublishTrimmed=false -o ./publish/conformu.windows-DotNet86
+signtool sign /a /v /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-DotNet86\*.dll"
+signtool sign /a /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-DotNet86\*.exe"
 7z a publish/conformu-windows-DotNet86.zip publish\conformu.windows-DotNet86\
 rmdir /s /q "publish/conformu.windows-DotNet86"
 
 dotnet publish -c Debug /p:Platform="Any CPU" -r win-x64 --framework net6.0-windows --self-contained false /p:PublishTrimmed=false -o ./publish/conformu.windows-DotNet64
+signtool sign /a /v /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-DotNet64\*.dll"
+signtool sign /a /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\conformu.windows-DotNet64\*.exe"
 7z a publish/conformu-windows-DotNet64.zip publish\conformu.windows-DotNet64\
 rmdir /s /q "publish/conformu.windows-DotNet64"
 
