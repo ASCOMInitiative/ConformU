6ebe123e4d6a7f0354de1dabc548682a5ecaaaf3
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-11-25 11:01:24 +0000

    Improve resilience of circuit handling

--- CircuitHandlerService.cs
+++ CircuitHandlerService.cs
@@ -12,13 +12,22 @@ namespace ConformU
     {
         private readonly IHostApplicationLifetime lifetime = null; // This is required if the StopApplication method is used.
         readonly ILogger<CircuitHandlerService> logger;
+        readonly object connectionsLockObject = new();
 
         private readonly List<string> connections;
         public CircuitHandlerService(IHostApplicationLifetime lifetime, ILogger<CircuitHandlerService> logger)
         {
             this.lifetime = lifetime; // This is required if the StopApplication method is used.
             this.logger = logger;
-            connections = new();
+
+            // Create a new connections object if required
+            if (connections is null)
+            {
+                lock (connectionsLockObject)
+                {
+                    connections = new();
+                }
+            }
         }
 
         public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
@@ -35,53 +44,59 @@ namespace ConformU
 
         public override Task OnCircuitOpenedAsync(Circuit circuit, CancellationToken cancellationToken)
         {
-            try
+            lock (connectionsLockObject)
             {
-                // Add the circuit to the list of circuits if not already in the list (it shouldn't be!)
-                if (!connections.Contains(circuit.Id))
+                try
                 {
-                    logger.LogInformation("***** OnCircuitOpenedAsync - Adding connection {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
-                    connections.Add(circuit.Id);
-                    logger.LogInformation("***** OnCircuitOpenedAsync - Added connection {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
+                    // Add the circuit to the list of circuits if not already in the list (it shouldn't be!)
+                    if (!connections.Contains(circuit.Id))
+                    {
+                        logger.LogInformation("***** OnCircuitOpenedAsync - Adding connection {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
+                        connections.Add(circuit.Id);
+                        logger.LogInformation("***** OnCircuitOpenedAsync - Added connection {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.LogInformation("***** OnCircuitOpenedAsync {circuit.Id} *****\r\n {ex}", circuit.Id, ex);
-            }
+                catch (Exception ex)
+                {
+                    logger.LogInformation("***** OnCircuitOpenedAsync {circuit.Id} *****\r\n {ex}", circuit.Id, ex);
+                }
 
-            return Task.CompletedTask;
+                return Task.CompletedTask;
+            }
         }
 
         public override Task OnCircuitClosedAsync(Circuit circuit, CancellationToken cancellationToken)
         {
             // Include a short delay to allow any new circuits to establish themselves before checking whether the application should close down
-            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
-            try
+            Task.Delay(TimeSpan.FromSeconds(1),cancellationToken).Wait(cancellationToken);
+            lock (connectionsLockObject)
             {
-                // Remove the circuit from the circuit list if present (it should be!)
-                if (connections.Contains(circuit.Id))
+                try
                 {
-                    logger.LogInformation("***** OnCircuitClosedAsync - Removing connection {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
-                    bool success = connections.Remove(circuit.Id);
-                    logger.LogInformation("***** OnCircuitClosedAsync - Removed connection {circuit.Id} Connection count: {connections.Count}, Success: {success} *****", circuit.Id, connections.Count, success);
-                }
+                    // Remove the circuit from the circuit list if present (it should be!)
+                    if (connections.Contains(circuit.Id))
+                    {
+                        logger.LogInformation("***** OnCircuitClosedAsync - Removing connection {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
+                        bool success = connections.Remove(circuit.Id);
+                        logger.LogInformation("***** OnCircuitClosedAsync - Removed connection {circuit.Id} Connection count: {connections.Count}, Success: {success} *****", circuit.Id, connections.Count, success);
+                    }
 
-                logger.LogInformation("***** OnCircuitClosedAsync - Before testing connection count: {connections.Count} *****", connections.Count);
-                // End the application if all circuits are closed
-                if (connections.Count == 0)
+                    logger.LogInformation("***** OnCircuitClosedAsync - Before testing connection count: {connections.Count} *****", connections.Count);
+                    // End the application if all circuits are closed
+                    if (connections.Count == 0)
+                    {
+                        logger.LogInformation("***** OnCircuitClosedAsync - Calling StopApplication - {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
+                        lifetime.StopApplication();
+                    }
+                }
+                catch (Exception ex)
                 {
-                    logger.LogInformation("***** OnCircuitClosedAsync - Calling StopApplication - {circuit.Id} Connection count: {connections.Count} *****", circuit.Id, connections.Count);
-                    lifetime.StopApplication();
+                    logger.LogInformation("***** OnCircuitClosedAsync {circuit.Id} *****\r\n {ex}", circuit.Id, ex);
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.LogInformation("***** OnCircuitClosedAsync {circuit.Id} *****\r\n {ex}", circuit.Id, ex);
-            }
-            logger.LogInformation("***** OnCircuitClosedAsync - OnCircuitClosedAsync. Connection count: {connections.Count} *****", connections.Count);
+                logger.LogInformation("***** OnCircuitClosedAsync - OnCircuitClosedAsync. Connection count: {connections.Count} *****", connections.Count);
 
-            return Task.CompletedTask;
+                return Task.CompletedTask;
+            }
         }
     }
 
---
+++ publishWindows.cmd
@@ -0,0 +1,31 @@
+echo *** Setup environment
+
+rmdir /s /q "publish"
+mkdir publish
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+cd
+cd J:\ConformU
+
+echo *** Build application
+MSBuild "J:\ConformU\ConformU.sln" /p:Configuration=Debug /p:Platform="Any CPU" /t:Restore 
+cd
+MSBuild "J:\ConformU\ConformU.sln" /p:Configuration=Debug /p:Platform="Any CPU" /t:Rebuild
+echo *** Completed Build
+
+echo *** Publishing Windows 64bit
+dotnet publish -c Debug /p:Platform="Any CPU" -r win-x64 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=true -o ./publish/ConformU64
+echo ***Completed 64bit publish
+
+echo *** Publishing Windows 32bit
+dotnet publish -c Debug /p:Platform="Any CPU" -r win-x86 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=true -o ./publish/ConformU86
+echo *** Completed 32bit publish
+
+echo *** Creating Windows installer
+cd setup
+"C:\Program Files (x86)\Inno Script Studio\isstudio.exe" -compile "J:\ConformU\Setup\ConformU.iss"
+cd ..
+
+echo *** Builds complete
+
+pause
