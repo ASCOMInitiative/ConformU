9e5fe9e5676aa9cfbc1e68cc68593c4a8b54e0a1
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-11-27 19:33:43 +0000

    Improve build process and publish all as single file applications.

--- publish.cmd
+++ publish.cmd
@@ -14,22 +14,12 @@ MSBuild "J:\ConformU\ConformU.sln" /p:Configuration=Debug /p:Platform="Any CPU" 
 echo *** Completed Build
 
 echo *** Publishing Windows 64bit
-dotnet publish -c Debug /p:Platform="Any CPU" -r win-x64 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false -o ./publish/ConformU64
+dotnet publish -c Debug /p:Platform="Any CPU" -r win-x64 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=true -o ./publish/ConformU64
 echo ***Completed 64bit publish
 
-echo *** Signing Windows 64bit
-rem signtool sign /a /as /v /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\ConformU64\*.dll"
-rem signtool sign /a /as /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\ConformU64\*.exe"
-echo ***Completed 64bit signing
-
 echo *** Publishing Windows 32bit
-dotnet publish -c Debug /p:Platform="Any CPU" -r win-x86 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false -o ./publish/ConformU86
+dotnet publish -c Debug /p:Platform="Any CPU" -r win-x86 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=true -o ./publish/ConformU86
 echo *** Completed 32bit publish
-
-echo *** Signing Windows 32bit
-rem signtool sign /a /as /v /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\ConformU86\*.dll"
-rem signtool sign /a /as /fd SHA256 /tr http://time.certum.pl /td SHA256 "publish\ConformU86\*.exe"
-echo *** Completed 32bit signing
 
 echo *** Creating Windows installer
 cd setup
@@ -47,17 +37,17 @@ bsdtar -cJf publish/conformu.macos-arm64.tar.xz -C publish\conformu.macos-arm64\
 echo *** Completed MacOS Apple silicon
 
 echo *** Publishing Linux ARM32
-dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm --framework net7.0 --self-contained true -o ./publish/conformu.linux-arm32 
+dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm --framework net7.0 --self-contained true /p:PublishSingleFile=true -o ./publish/conformu.linux-arm32 
 bsdtar -cJf publish/conformu.linux-arm32.needsexec.tar.xz -C publish\conformu.linux-arm32 *
 echo *** Completed Linux ARM32
 
 echo *** Publishing Linux ARM64
-dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm64 --framework net7.0 --self-contained true -o ./publish/conformu.linux-arm64
+dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm64 --framework net7.0 --self-contained true /p:PublishSingleFile=true -o ./publish/conformu.linux-arm64
 bsdtar -cJf publish/conformu.linux-arm64.needsexec.tar.xz -C publish\conformu.linux-arm64 *
 echo *** Completed Linux ARM64
 
 echo *** Publishing Linux X64
-dotnet publish -c Debug /p:Platform="Any CPU" -r linux-x64 --framework net7.0 --self-contained true -o ./publish/conformu.linux-x64
+dotnet publish -c Debug /p:Platform="Any CPU" -r linux-x64 --framework net7.0 --self-contained true /p:PublishSingleFile=true -o ./publish/conformu.linux-x64
 bsdtar -cJf publish/conformu.linux-x64.needsexec.tar.xz -C publish\conformu.linux-x64\ *
 echo *** Completed Linux X64
 
