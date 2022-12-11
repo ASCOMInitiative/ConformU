0976d30c11b00e93128b1e4492054647e2ed8674
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-12-11 12:53:50 +0000

    Add discovery diagnostics outcome summary

---
+++ ConformU/Conform/DataClasses/AlpacaDiscoveryDiagnostic.cs
@@ -0,0 +1,23 @@
+﻿namespace ConformU
+{
+    /// <summary>
+    /// Information or issue message shown when running Alpaca discovery diagnostics
+    /// </summary>
+    public class AlpacaDiscoveryDiagnostic
+    {
+        // Create a new diagnostic message list entry
+        public AlpacaDiscoveryDiagnostic(string deviceIp,string message)
+        {
+            DeviceIp = deviceIp;
+            Message= message;
+        }
+
+        /// <summary>
+        ///  Device IP address
+        /// </summary>
+        public string DeviceIp { get; set; }
+
+        // Message shown
+        public string Message { get; set; }
+    }
+}
