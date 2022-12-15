43473fe1806290148949d57c9fa9d496cdfa263b
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-12-15 14:04:09 +0000

    Add Alpaca protocol checking feature(common methods and Telescope only)

---
+++ ConformU/CheckProtocolParameter.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConformU
+{
+    public class CheckProtocolParameter
+    {
+        public CheckProtocolParameter(string parameterName, string parameterValue)
+        {
+            ParameterName = parameterName;
+            ParameterValue = parameterValue;
+        }
+
+
+        public string ParameterName { get; set; } = string.Empty;
+
+        public string ParameterValue { get; set; } = string.Empty;
+    }
+}
--- ConformU/Conform/DataClasses/AlpacaConfiguration.cs
+++ ConformU/Conform/DataClasses/AlpacaConfiguration.cs
@@ -23,5 +23,6 @@ namespace ConformU
         public int EstablishConnectionTimeout { get; set; } = 5;
         public int StandardResponseTimeout{ get; set; } = 10;
         public int LongResponseTimeout{ get; set; } = 100;
+        public bool ProtocolShowSuccessResponses { get; set; } = true;
     }
 }
