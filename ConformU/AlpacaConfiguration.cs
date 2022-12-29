047bd29403191185650d15cf6ad107dbcc95d94c
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-12-29 11:28:55 +0000

    Alpaca protocol: Improve ASCOM error reporting

--- ConformU/AlpacaConfiguration.cs
+++ ConformU/AlpacaConfiguration.cs
@@ -24,5 +24,6 @@ namespace ConformU
         public int StandardResponseTimeout{ get; set; } = 10;
         public int LongResponseTimeout{ get; set; } = 100;
         public bool ProtocolShowSuccessResponses { get; set; } = true;
+        public bool ProtocolReportNotImplementedErrors { get; set; } = false;
     }
 }
