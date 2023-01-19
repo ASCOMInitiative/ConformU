2ea332af37f3ecb0efc2fa765031783c49de3b3f
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-01-19 19:30:21 +0000

    Make stop test handler asynchronous. Add .NET logging component.

---
+++ ConformU/NetworkLogger.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+using System;
+using ASCOM.Tools;
+using Microsoft.Extensions.Logging;
+
+namespace ConformU
+{
+    /// <summary>
+    /// Class to make .NET internal log messages appear on the console and in the trace logger log.
+    /// </summary>
+    internal sealed class NetworkLogger : EventListener
+    {
+        // Constant necessary for attaching ActivityId to the events.
+        public const EventKeywords TasksFlowActivityIds = (EventKeywords)0x80;
+        private readonly TraceLogger logger;
+
+        internal NetworkLogger(TraceLogger logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            // List of event source names provided by networking in .NET 5.
+            if (eventSource.Name == "System.Net.Http" ||
+                eventSource.Name == "System.Net.Sockets" ||
+                eventSource.Name == "System.Net.Security" ||
+                eventSource.Name == "System.Net.NameResolution")
+            {
+                EnableEvents(eventSource, EventLevel.LogAlways);
+            }
+            // Turn on ActivityId.
+            else if (eventSource.Name == "System.Threading.Tasks.TplEventSource")
+            {
+                // Attach ActivityId to the events.
+                EnableEvents(eventSource, EventLevel.Verbose, TasksFlowActivityIds);
+            }
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            var sb = new StringBuilder().Append($"{eventData.EventSource.Name}.{eventData.EventName}(");
+            for (int i = 0; i < eventData.Payload?.Count; i++)
+            {
+                sb.Append(eventData.PayloadNames?[i]).Append(": ").Append(eventData.Payload[i]);
+                if (i < eventData.Payload?.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            sb.Append(')');
+            Console.WriteLine(sb.ToString());
+            logger.LogMessage("NetworkLogger", sb.ToString());
+        }
+    }
+}
