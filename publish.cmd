cad36e1935f8e957edd7fb089b21c94aa4b16c86
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-05-01 09:03:08 +0100

    Re-work publish command.

--- publish.cmd
+++ publish.cmd
@@ -13,6 +13,31 @@ cd
 MSBuild "J:\ConformU\ConformU.sln" /p:Configuration=Debug /p:Platform="Any CPU" /t:Rebuild
 echo *** Completed Build
 
+echo *** Publishing MacOS Intel silicon
+dotnet publish -c Debug -p:Platform="Any CPU" -r osx-x64 --framework net7.0 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRunShowWarnings=true
+bsdtar -cJf publish/conformu.macos-x64.tar.xz -C ConformU\bin\Debug\net7.0\osx-x64\publish\ *
+echo *** Completed MacOS Intel silicon
+
+echo *** Publishing MacOS Apple silicon
+dotnet publish -c Debug -p:Platform="Any CPU" -r osx-arm64 --framework net7.0 --self-contained true -p:PublishSingleFile=true 
+bsdtar -cJf publish/conformu.macos-arm64.tar.xz -C ConformU\bin\Debug\net7.0\osx-arm64\publish\ *
+echo *** Completed MacOS Apple silicon
+
+echo *** Publishing Linux ARM32
+dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm --framework net7.0 --self-contained true /p:PublishSingleFile=true 
+bsdtar -cJf publish/conformu.linux-arm32.needsexec.tar.xz -C ConformU\bin\Debug\net7.0\linux-arm\publish\ *
+echo *** Completed Linux ARM32
+
+echo *** Publishing Linux ARM64
+dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm64 --framework net7.0 --self-contained true /p:PublishSingleFile=true
+bsdtar -cJf publish/conformu.linux-arm64.needsexec.tar.xz -C ConformU\bin\Debug\net7.0\linux-arm64\publish\ *
+echo *** Completed Linux ARM64
+
+echo *** Publishing Linux X64
+dotnet publish -c Debug /p:Platform="Any CPU" -r linux-x64 --framework net7.0 --self-contained true /p:PublishSingleFile=true
+bsdtar -cJf publish/conformu.linux-x64.needsexec.tar.xz -C ConformU\bin\Debug\net7.0\linux-x64\publish\ *
+echo *** Completed Linux X64
+
 echo *** Publishing Windows 64bit
 dotnet publish ConformU/ConformU.csproj -c Debug /p:Platform="Any CPU" -r win-x64 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=false -o ./publish/ConformU64
 echo ***Completed 64bit publish
@@ -29,32 +54,5 @@ cd WindowsInstaller
 "C:\Program Files (x86)\Inno Script Studio\isstudio.exe" -compile "J:\ConformU\WindowsInstaller\ConformU.iss"
 cd ..
 
-
-echo *** Publishing MacOS Intel silicon
-dotnet publish -c Debug -p:Platform="Any CPU" -r osx-x64 --framework net7.0 --self-contained true -o ./publish/conformu.macos-x64 -p:PublishSingleFile=true -p:PublishReadyToRunShowWarnings=true
-bsdtar -cJf publish/conformu.macos-x64.tar.xz -C publish\conformu.macos-x64\ *
-echo *** Completed MacOS Intel silicon
-
-echo *** Publishing MacOS Apple silicon
-dotnet publish -c Debug -p:Platform="Any CPU" -r osx-arm64 --framework net7.0 --self-contained true -o ./publish/conformu.macos-arm64 -p:PublishSingleFile=true 
-bsdtar -cJf publish/conformu.macos-arm64.tar.xz -C publish\conformu.macos-arm64\ *
-echo *** Completed MacOS Apple silicon
-
-echo *** Publishing Linux ARM32
-dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm --framework net7.0 --self-contained true /p:PublishSingleFile=true -o ./publish/conformu.linux-arm32 
-bsdtar -cJf publish/conformu.linux-arm32.needsexec.tar.xz -C publish\conformu.linux-arm32 *
-echo *** Completed Linux ARM32
-
-echo *** Publishing Linux ARM64
-dotnet publish -c Debug /p:Platform="Any CPU" -r linux-arm64 --framework net7.0 --self-contained true /p:PublishSingleFile=true -o ./publish/conformu.linux-arm64
-bsdtar -cJf publish/conformu.linux-arm64.needsexec.tar.xz -C publish\conformu.linux-arm64 *
-echo *** Completed Linux ARM64
-
-echo *** Publishing Linux X64
-dotnet publish -c Debug /p:Platform="Any CPU" -r linux-x64 --framework net7.0 --self-contained true /p:PublishSingleFile=true -o ./publish/conformu.linux-x64
-bsdtar -cJf publish/conformu.linux-x64.needsexec.tar.xz -C publish\conformu.linux-x64\ *
-echo *** Completed Linux X64
-
 echo *** Builds complete
-
 pause
