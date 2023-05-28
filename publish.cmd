d229f38f6d2a7253a687775f0921851433d037e9
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-05-28 08:56:36 +0100

    Publish Windows to a single file.

--- publish.cmd
+++ publish.cmd
@@ -39,11 +39,11 @@ bsdtar -cJf publish/conformu.linux-x64.needsexec.tar.xz -C ConformU\bin\Debug\ne
 echo *** Completed Linux X64
 
 echo *** Publishing Windows 64bit
-dotnet publish ConformU/ConformU.csproj -c Debug /p:Platform="Any CPU" -r win-x64 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=false -o ./publish/ConformU64
+dotnet publish ConformU/ConformU.csproj -c Debug /p:Platform="Any CPU" -r win-x64 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=strue -o ./publish/ConformU64
 echo ***Completed 64bit publish
 
 echo *** Publishing Windows 32bit
-dotnet publish ConformU/ConformU.csproj -c Debug /p:Platform="Any CPU" -r win-x86 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=false -o ./publish/ConformU86
+dotnet publish ConformU/ConformU.csproj -c Debug /p:Platform="Any CPU" -r win-x86 --framework net7.0-windows --self-contained true /p:PublishTrimmed=false /p:PublishSingleFile=true -o ./publish/ConformU86
 echo *** Completed 32bit publish
 
 editbin /LARGEADDRESSAWARE ./publish/ConformU86/Conformu.exe
